ce585e023023bf7cd149a186c7ad0d9f155ff6d8
wxz <759126349@qq.com>
2022-12-26 22:22:58 +0800

    优化代码

--- src/main/java/com/example/interview/juc/FutureApiDemo.java
+++ src/main/java/com/example/interview/juc/FutureApiDemo.java
@@ -1,12 +1,38 @@
 package com.example.interview.juc;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 /**
  *
  * @author wxz
  * @date 21:56 2022/12/26
  */
 public class FutureApiDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+
+        FutureTask<String> futureTask = new FutureTask<>(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t ------- come in");
+            TimeUnit.SECONDS.sleep(5);
+            return "task over";
+        });
+
+        Thread t1 = new Thread(futureTask, "t1");
+        t1.start();
+
+        System.out.println(Thread.currentThread().getName() + "\t 忙其他任务了");
+
+        while (true) {
+            if (futureTask.isDone()) {
+                System.out.println(futureTask.get());
+                break;
+            } else {
+                TimeUnit.MILLISECONDS.sleep(500);
+                System.out.println("正在处理中...");
+            }
+        }
 
     }
 }
