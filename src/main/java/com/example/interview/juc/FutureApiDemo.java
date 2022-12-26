5b5a14167f7c2ca9a1980871fe3d59448ce5c41e
wxz <759126349@qq.com>
2022-12-26 21:56:26 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/FutureApiDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc;
+
+/**
+ *
+ * @author wxz
+ * @date 21:56 2022/12/26
+ */
+public class FutureApiDemo {
+    public static void main(String[] args) {
+
+    }
+}
