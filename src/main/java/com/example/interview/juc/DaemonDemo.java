d80e4d0bac28bb7c182a0d222044e248e56fae10
wxz <759126349@qq.com>
2022-12-26 14:58:34 +0800

    优化代码

--- src/main/java/com/example/interview/juc/DaemonDemo.java
+++ src/main/java/com/example/interview/juc/DaemonDemo.java
@@ -1,9 +1,28 @@
 package com.example.interview.juc;
 
+import java.util.concurrent.TimeUnit;
+
 /**
- *
  * @author wxz
  * @date 14:47 2022/12/26
  */
 public class DaemonDemo {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t 开始执行" +
+                    (Thread.currentThread().isDaemon() ? "守护线程" : "用户线程"));
+            while (true) {}
+        }, "t1");
+
+        t1.setDaemon(true);
+        t1.start();
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(Thread.currentThread().getName() + "\t   ------ end 主线程");
+    }
 }
