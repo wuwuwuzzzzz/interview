6755472fe45722e4c86fce777cd9721fb9e8aea7
wxz <759126349@qq.com>
2022-12-26 21:36:55 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/FutureThreadPoolDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc;
+
+/**
+ *
+ * @author wxz
+ * @date 21:36 2022/12/26
+ */
+public class FutureThreadPoolDemo {
+    public static void main(String[] args) {
+
+    }
+}
