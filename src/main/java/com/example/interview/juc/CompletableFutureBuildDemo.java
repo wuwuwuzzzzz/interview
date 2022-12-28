3d3ee11fe7726de0178934144e373fd4152d535c
wxz <759126349@qq.com>
2022-12-28 21:49:48 +0800

    优化代码

--- src/main/java/com/example/interview/juc/CompletableFutureBuildDemo.java
+++ src/main/java/com/example/interview/juc/CompletableFutureBuildDemo.java
@@ -1,9 +1,28 @@
 package com.example.interview.juc;
 
+import java.util.concurrent.*;
+
 /**
  *
  * @author wxz
  * @date 21:37 2022/12/28
  */
 public class CompletableFutureBuildDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+
+        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(() -> {
+            System.out.println(Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, threadPool);
+
+        System.out.println(voidCompletableFuture.get());
+
+        threadPool.shutdown();
+    }
 }
