cbd57213f8695d9f7bbedf44990a4855143faf1d
wxz <759126349@qq.com>
2022-12-28 22:25:51 +0800

    优化代码

--- src/main/java/com/example/interview/juc/CompletableFutureBuildDemo.java
+++ src/main/java/com/example/interview/juc/CompletableFutureBuildDemo.java
@@ -23,6 +23,18 @@ public class CompletableFutureBuildDemo {
 
         System.out.println(voidCompletableFuture.get());
 
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "supplyAsync";
+        }, threadPool);
+
+        System.out.println(completableFuture.get());
+
         threadPool.shutdown();
     }
 }
