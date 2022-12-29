b058cd0556309e153bcc7c4e50e085294da13b57
wxz <759126349@qq.com>
2022-12-29 16:48:18 +0800

    优化代码

--- src/main/java/com/example/interview/juc/CompletableFutureThreadPoolDemo.java
+++ src/main/java/com/example/interview/juc/CompletableFutureThreadPoolDemo.java
@@ -1,12 +1,55 @@
 package com.example.interview.juc;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
- *
  * @author wxz
  * @date 16:38 2022/12/29
  */
 public class CompletableFutureThreadPoolDemo {
     public static void main(String[] args) {
 
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+
+        try {
+            CompletableFuture<Void> completableFuture = CompletableFuture.supplyAsync(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(20);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("1号任务" + Thread.currentThread().getName());
+                return "abc";
+            }, threadPool).thenRun(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(20);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("2号任务" + Thread.currentThread().getName());
+            }).thenRun(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("3号任务" + Thread.currentThread().getName());
+            }).thenRun(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("4号任务" + Thread.currentThread().getName());
+            });
+            System.out.println(completableFuture.get(2L, TimeUnit.SECONDS));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
     }
 }
