932c89d93afadda06cade1ba60b594c20cfb91e1
wxz <759126349@qq.com>
2022-12-29 17:00:12 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/CompletableFutureFastDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc;
+
+/**
+ *
+ * @author wxz
+ * @date 16:59 2022/12/29
+ */
+public class CompletableFutureFastDemo {
+    public static void main(String[] args) {
+
+    }
+}
