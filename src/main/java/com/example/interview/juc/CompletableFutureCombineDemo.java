dff8977febbc70976140ff53c75647deb4c19996
wxz <759126349@qq.com>
2022-12-29 17:07:43 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/CompletableFutureCombineDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc;
+
+/**
+ *
+ * @author wxz
+ * @date 17:07 2022/12/29
+ */
+public class CompletableFutureCombineDemo {
+    public static void main(String[] args) {
+
+    }
+}
