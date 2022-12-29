7ab92662d5d54382da03781f2b7728e3657cb9dc
wxz <759126349@qq.com>
2022-12-29 16:38:37 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/CompletableFutureThreadPoolDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc;
+
+/**
+ *
+ * @author wxz
+ * @date 16:38 2022/12/29
+ */
+public class CompletableFutureThreadPoolDemo {
+    public static void main(String[] args) {
+
+    }
+}
