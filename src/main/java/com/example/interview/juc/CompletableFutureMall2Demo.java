893b58079d3f0d1b602c432f2dc9762bf145d049
wxz <759126349@qq.com>
2022-12-29 14:39:44 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/CompletableFutureMall2Demo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc;
+
+/**
+ *
+ * @author wxz
+ * @date 14:39 2022/12/29
+ */
+public class CompletableFutureMall2Demo {
+    public static void main(String[] args) {
+
+    }
+}
