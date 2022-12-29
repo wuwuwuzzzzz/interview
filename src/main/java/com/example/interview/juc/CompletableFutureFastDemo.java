477032c5093cedf55cb974486f733426343ca4e8
wxz <759126349@qq.com>
2022-12-29 17:07:01 +0800

    优化代码

--- src/main/java/com/example/interview/juc/CompletableFutureFastDemo.java
+++ src/main/java/com/example/interview/juc/CompletableFutureFastDemo.java
@@ -1,5 +1,8 @@
 package com.example.interview.juc;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author wxz
@@ -8,5 +11,32 @@ package com.example.interview.juc;
 public class CompletableFutureFastDemo {
     public static void main(String[] args) {
 
+        CompletableFuture<String> playA = CompletableFuture.supplyAsync(() -> {
+
+            System.out.println("A come in");
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "playA";
+        });
+
+        CompletableFuture<String> playB = CompletableFuture.supplyAsync(() -> {
+
+            System.out.println("B come in");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "playB";
+        });
+
+        CompletableFuture<String> result = playA.applyToEither(playB, f -> {
+            return f + " is win";
+        });
+
+        System.out.println(Thread.currentThread().getName() + "\t" + "-------" + result.join());
     }
 }
