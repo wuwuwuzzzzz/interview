fb6d5202681cf36da8a826d8a08da27c3360a1c5
wxz <759126349@qq.com>
2022-12-29 15:01:08 +0800

    优化代码

--- src/main/java/com/example/interview/juc/CompletableFutureMall2Demo.java
+++ src/main/java/com/example/interview/juc/CompletableFutureMall2Demo.java
@@ -7,9 +7,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
- *
  * @author wxz
  * @date 14:39 2022/12/29
  */
@@ -22,8 +22,22 @@ public class CompletableFutureMall2Demo {
 
     );
 
+    public static List<String> getPrice(List<NetMall> list, String productName) {
+        return list.stream().map(netMall -> String.format(productName + " in %s price is %.2f", netMall.getNetMallName(), netMall.calcPrice(productName))).collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
+
         System.out.println(ThreadLocalRandom.current().nextDouble() * 2 + "mysql".charAt(0));
+
+        long startTime = System.currentTimeMillis();
+        List<String> list1 = getPrice(list, "mysql");
+        for (String element : list1) {
+            System.out.println(element);
+        }
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("耗时: " + (endTime - startTime) + "毫秒");
     }
 }
 
