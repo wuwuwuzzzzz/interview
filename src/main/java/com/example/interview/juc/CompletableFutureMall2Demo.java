4657b52a6fd261be17307b4b24923dfcd646fccd
wxz <759126349@qq.com>
2022-12-29 14:49:50 +0800

    优化代码

--- src/main/java/com/example/interview/juc/CompletableFutureMall2Demo.java
+++ src/main/java/com/example/interview/juc/CompletableFutureMall2Demo.java
@@ -1,12 +1,50 @@
 package com.example.interview.juc;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author wxz
  * @date 14:39 2022/12/29
  */
 public class CompletableFutureMall2Demo {
-    public static void main(String[] args) {
 
+    static List<NetMall> list = Arrays.asList(
+            new NetMall("jd"),
+            new NetMall("dd"),
+            new NetMall("tb")
+
+    );
+
+    public static void main(String[] args) {
+        System.out.println(ThreadLocalRandom.current().nextDouble() * 2 + "mysql".charAt(0));
+    }
+}
+
+@AllArgsConstructor
+class NetMall {
+
+    @Getter
+    private String netMallName;
+
+    public void setNetMallName(String netMallName) {
+        this.netMallName = netMallName;
+    }
+
+    public double calcPrice(String productName) {
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ThreadLocalRandom.current().nextDouble() * 2 + productName.charAt(0);
     }
 }
