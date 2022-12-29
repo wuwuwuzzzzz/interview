c6dbdebf6ee06b401946d5d00e60aaed01fabc65
wxz <759126349@qq.com>
2022-12-29 15:52:04 +0800

    优化代码

--- src/main/java/com/example/interview/juc/CompletableFutureApi3Demo.java
+++ src/main/java/com/example/interview/juc/CompletableFutureApi3Demo.java
@@ -1,5 +1,7 @@
 package com.example.interview.juc;
 
+import java.util.concurrent.CompletableFuture;
+
 /**
  *
  * @author wxz
@@ -7,6 +9,15 @@ package com.example.interview.juc;
  */
 public class CompletableFutureApi3Demo {
     public static void main(String[] args) {
-
+        CompletableFuture.supplyAsync(() -> {
+            System.out.println("111");
+            return 1;
+        }).thenApply(f -> {
+            System.out.println("222");
+            return f + 2;
+        }).thenApply(f -> {
+            System.out.println("333");
+            return f + 3;
+        }).thenAccept(System.out::println);
     }
 }
