29404a14b1a9fa17a9d3047bf1dfb8157de10aef
wxz <759126349@qq.com>
2022-12-29 15:17:58 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/CompletableFutureApiDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc;
+
+/**
+ *
+ * @author wxz
+ * @date 15:17 2022/12/29
+ */
+public class CompletableFutureApiDemo {
+    public static void main(String[] args) {
+
+    }
+}
