19145ff2f2694bc36909adee0a42bb1bc68ea33e
wxz <759126349@qq.com>
2022-12-29 17:11:12 +0800

    优化代码

--- src/main/java/com/example/interview/juc/CompletableFutureCombineDemo.java
+++ src/main/java/com/example/interview/juc/CompletableFutureCombineDemo.java
@@ -1,5 +1,8 @@
 package com.example.interview.juc;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author wxz
@@ -8,5 +11,31 @@ package com.example.interview.juc;
 public class CompletableFutureCombineDemo {
     public static void main(String[] args) {
 
+        CompletableFuture<Integer> completableFuture1 = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t" + "启动");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return 10;
+        });
+
+        CompletableFuture<Integer> completableFuture2 = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t" + "启动");
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return 20;
+        });
+
+        CompletableFuture<Integer> result = completableFuture1.thenCombine(completableFuture2, (x, y) -> {
+            System.out.println("开始合并");
+            return x + y;
+        });
+
+        System.out.println(result.join());
     }
 }
