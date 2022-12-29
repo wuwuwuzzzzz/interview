2644912619fef9e3f93d4f33152f978cdd6277a6
wxz <759126349@qq.com>
2022-12-29 15:11:19 +0800

    优化代码

--- src/main/java/com/example/interview/juc/CompletableFutureMall2Demo.java
+++ src/main/java/com/example/interview/juc/CompletableFutureMall2Demo.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -26,6 +27,12 @@ public class CompletableFutureMall2Demo {
         return list.stream().map(netMall -> String.format(productName + " in %s price is %.2f", netMall.getNetMallName(), netMall.calcPrice(productName))).collect(Collectors.toList());
     }
 
+    public static List<String> getPriceByCompletableFuture(List<NetMall> list, String productName) {
+        return list.stream().map(netMall -> CompletableFuture.supplyAsync(() ->
+            String.format(productName + " in %s price is %.2f", netMall.getNetMallName(), netMall.calcPrice(productName))
+        )).collect(Collectors.toList()).stream().map(CompletableFuture::join).collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
 
         System.out.println(ThreadLocalRandom.current().nextDouble() * 2 + "mysql".charAt(0));
@@ -38,6 +45,15 @@ public class CompletableFutureMall2Demo {
         long endTime = System.currentTimeMillis();
 
         System.out.println("耗时: " + (endTime - startTime) + "毫秒");
+
+        long startTime1 = System.currentTimeMillis();
+        List<String> list2 = getPriceByCompletableFuture(list, "mysql");
+        for (String element : list2) {
+            System.out.println(element);
+        }
+        long endTime1 = System.currentTimeMillis();
+
+        System.out.println("耗时: " + (endTime1 - startTime1) + "毫秒");
     }
 }
 
