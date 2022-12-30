c0fffba58ea3d6ecc483589844a23b4f90999974
wxz <759126349@qq.com>
2022-12-30 16:28:49 +0800

    优化代码

--- src/main/java/com/example/interview/juc/lock/ReEntryLockDemo.java
+++ src/main/java/com/example/interview/juc/lock/ReEntryLockDemo.java
@@ -8,5 +8,18 @@ package com.example.interview.juc.lock;
 public class ReEntryLockDemo {
     public static void main(String[] args) {
 
+        final Object object = new Object();
+
+        new Thread(() -> {
+            synchronized (object) {
+                System.out.println(Thread.currentThread().getName() + "\t ------- 外层调用");
+                synchronized (object) {
+                    System.out.println(Thread.currentThread().getName() + "\t ------- 中层调用");
+                    synchronized (object) {
+                        System.out.println(Thread.currentThread().getName() + "\t ------- 内层调用");
+                    }
+                }
+            }
+        }, "t1").start();
     }
 }
