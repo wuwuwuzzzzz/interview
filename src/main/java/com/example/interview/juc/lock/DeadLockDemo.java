09a05185b9e19030694ca6dfa9eed12bd34a5295
wxz <759126349@qq.com>
2022-12-30 17:00:09 +0800

    优化代码

--- src/main/java/com/example/interview/juc/lock/DeadLockDemo.java
+++ src/main/java/com/example/interview/juc/lock/DeadLockDemo.java
@@ -1,5 +1,7 @@
 package com.example.interview.juc.lock;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author wxz
@@ -8,5 +10,36 @@ package com.example.interview.juc.lock;
 public class DeadLockDemo {
     public static void main(String[] args) {
 
+        final Object objectA = new Object();
+
+        final Object objectB = new Object();
+
+        new Thread(() -> {
+            synchronized (objectA) {
+                System.out.println(Thread.currentThread().getName() + "\t 自己持有A锁，希望获取B锁");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                synchronized (objectB) {
+                    System.out.println(Thread.currentThread().getName() + "\t 成功获取B锁");
+                }
+            }
+        }, "A").start();
+
+        new Thread(() -> {
+            synchronized (objectB) {
+                System.out.println(Thread.currentThread().getName() + "\t 自己持有B锁，希望获取A锁");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                synchronized (objectA) {
+                    System.out.println(Thread.currentThread().getName() + "\t 成功获取A锁");
+                }
+            }
+        }, "B").start();
     }
 }
