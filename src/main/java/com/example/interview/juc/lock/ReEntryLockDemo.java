25f6828cdbbf3f87f5ef8c705a1f70f72f8e07e2
wxz <759126349@qq.com>
2022-12-30 16:33:08 +0800

    优化代码

--- src/main/java/com/example/interview/juc/lock/ReEntryLockDemo.java
+++ src/main/java/com/example/interview/juc/lock/ReEntryLockDemo.java
@@ -6,8 +6,33 @@ package com.example.interview.juc.lock;
  * @date 16:25 2022/12/30
  */
 public class ReEntryLockDemo {
+
+    public synchronized void m1() {
+        System.out.println(Thread.currentThread().getName() + "\t come in");
+        m2();
+        System.out.println(Thread.currentThread().getName() + "\t end");
+    }
+
+    public synchronized void m2() {
+        System.out.println(Thread.currentThread().getName() + "\t come in");
+        m3();
+        System.out.println(Thread.currentThread().getName() + "\t end");
+    }
+
+    public synchronized void m3() {
+        System.out.println(Thread.currentThread().getName() + "\t come in");
+    }
+
     public static void main(String[] args) {
 
+        ReEntryLockDemo reEntryLockDemo = new ReEntryLockDemo();
+
+        new Thread(() -> {
+            reEntryLockDemo.m1();
+        }, "t1").start();
+    }
+
+    private static void M1() {
         final Object object = new Object();
 
         new Thread(() -> {
