3f0027be8af410a822ed31a73cd45da3b9cf9063
wxz <759126349@qq.com>
2022-12-30 16:50:53 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/lock/DeadLockDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.lock;
+
+/**
+ *
+ * @author wxz
+ * @date 16:50 2022/12/30
+ */
+public class DeadLockDemo {
+    public static void main(String[] args) {
+
+    }
+}
