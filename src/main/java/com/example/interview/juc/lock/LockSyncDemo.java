c56e585522eb9c4ce69a766df654f7a3b4decca9
wxz <759126349@qq.com>
2022-12-30 11:54:54 +0800

    优化代码

--- src/main/java/com/example/interview/juc/lock/LockSyncDemo.java
+++ src/main/java/com/example/interview/juc/lock/LockSyncDemo.java
@@ -6,7 +6,23 @@ package com.example.interview.juc.lock;
  * @date 11:28 2022/12/30
  */
 public class LockSyncDemo {
-    public static void main(String[] args) {
 
+    Object object = new Object();
+
+    public void m1() {
+        synchronized (object) {
+            System.out.println("hello synchronized code block");
+        }
+    }
+
+    public synchronized void m2() {
+        System.out.println("hello synchronized code block");
+    }
+
+    public static synchronized void m3() {
+        System.out.println("hello synchronized code block");
+    }
+
+    public static void main(String[] args) {
     }
 }
