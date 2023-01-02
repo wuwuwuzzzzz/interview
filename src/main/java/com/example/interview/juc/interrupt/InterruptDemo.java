764184cf54aa687b4b6e14e85fd0430f73310dff
wxz <759126349@qq.com>
2023-01-02 14:47:11 +0800

    优化代码

--- src/main/java/com/example/interview/juc/interrupt/InterruptDemo.java
+++ src/main/java/com/example/interview/juc/interrupt/InterruptDemo.java
@@ -15,7 +15,7 @@ public class InterruptDemo {
     static AtomicBoolean atomicBoolean = new AtomicBoolean(false);
 
     public static void main(String[] args) {
-        m2();
+
     }
 
     private static void m2() {
