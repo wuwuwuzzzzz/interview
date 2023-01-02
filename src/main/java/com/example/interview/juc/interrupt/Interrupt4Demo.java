3ae539b83aeddcc5fa310e3344cf8e897c3f8955
wxz <759126349@qq.com>
2023-01-02 15:46:51 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/interrupt/Interrupt4Demo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.interrupt;
+
+/**
+ *
+ * @author wxz
+ * @date 15:46 2023/1/2
+ */
+public class Interrupt4Demo {
+    public static void main(String[] args) {
+
+    }
+}
