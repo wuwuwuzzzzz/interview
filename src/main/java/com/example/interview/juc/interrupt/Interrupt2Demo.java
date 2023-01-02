4db26e66fc696714ec3bb61d3fefa2d88c307ebb
wxz <759126349@qq.com>
2023-01-02 15:31:56 +0800

    优化代码

--- src/main/java/com/example/interview/juc/interrupt/Interrupt2Demo.java
+++ src/main/java/com/example/interview/juc/interrupt/Interrupt2Demo.java
@@ -1,5 +1,7 @@
 package com.example.interview.juc.interrupt;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author wxz
@@ -8,5 +10,32 @@ package com.example.interview.juc.interrupt;
 public class Interrupt2Demo {
     public static void main(String[] args) {
 
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 300; i++) {
+                System.out.println("-----------" + i);
+            }
+            System.out.println("t1线程调用interrupt()方法后的中断标识02: " + Thread.currentThread().isInterrupted());
+        }, "t1");
+        t1.start();
+
+        System.out.println("t1线程默认的中断标识: " + t1.isInterrupted());
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        t1.interrupt();
+
+        System.out.println("t1线程调用interrupt()方法后的中断标识01: " + t1.isInterrupted());
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("t1线程调用interrupt()方法后的中断标识03: " + t1.isInterrupted());
     }
 }
