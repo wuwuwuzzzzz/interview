a88010c3bd4eac16a170a6f481864e1f59ab4ca8
wxz <759126349@qq.com>
2023-01-02 14:44:48 +0800

    优化代码

--- src/main/java/com/example/interview/juc/interrupt/InterruptDemo.java
+++ src/main/java/com/example/interview/juc/interrupt/InterruptDemo.java
@@ -1,6 +1,7 @@
 package com.example.interview.juc.interrupt;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  *
@@ -11,16 +12,43 @@ public class InterruptDemo {
 
     static volatile boolean isStop = false;
 
-    public static void main(String[] args) {
+    static AtomicBoolean atomicBoolean = new AtomicBoolean(false);
 
+    public static void main(String[] args) {
+        m2();
+    }
+
+    private static void m2() {
+        new Thread(() -> {
+            while (true) {
+                if (atomicBoolean.get()) {
+                    System.out.println(Thread.currentThread().getName() + "\t atomicBoolean被修改位true，程序停止");
+                    break;
+                }
+                System.out.println("hello atomicBoolean");
+            }
+        }, "t1").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            atomicBoolean.set(true);
+        }, "t2").start();
+    }
+
+    private static void m1() {
         new Thread(() -> {
             while (true) {
                 if (isStop) {
                     System.out.println(Thread.currentThread().getName() + "\t isStop被修改位true，程序停止");
                     break;
                 }
+                System.out.println("hello volatile");
             }
-            System.out.println("hello volatile");
         }, "t1").start();
 
         try {
