e6077775b3974f992b0691e290aef453a563130b
wxz <759126349@qq.com>
2023-01-02 15:46:18 +0800

    优化代码

--- src/main/java/com/example/interview/juc/interrupt/Interrupt3Demo.java
+++ src/main/java/com/example/interview/juc/interrupt/Interrupt3Demo.java
@@ -1,5 +1,7 @@
 package com.example.interview.juc.interrupt;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author wxz
@@ -8,5 +10,34 @@ package com.example.interview.juc.interrupt;
 public class Interrupt3Demo {
     public static void main(String[] args) {
 
+        Thread t1 = new Thread(() -> {
+            while (true) {
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println(Thread.currentThread().getName() + "\t 中断标识位: " + Thread.currentThread().isInterrupted() + " 程序停止");
+                    break;
+                }
+
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException(e);
+                }
+
+                System.out.println("------ hello Interrupt3Demo");
+            }
+        }, "t1");
+        t1.start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        Thread t2 = new Thread(() -> {
+            t1.interrupt();
+        }, "t2");
+        t2.start();
     }
 }
