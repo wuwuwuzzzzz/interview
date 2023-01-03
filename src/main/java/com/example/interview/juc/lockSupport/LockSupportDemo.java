fe7a367b89c59caf99be42a471890d234703a9ff
wxz <759126349@qq.com>
2023-01-03 14:25:01 +0800

    优化代码

--- src/main/java/com/example/interview/juc/lockSupport/LockSupportDemo.java
+++ src/main/java/com/example/interview/juc/lockSupport/LockSupportDemo.java
@@ -1,5 +1,7 @@
 package com.example.interview.juc.lockSupport;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author wxz
@@ -8,5 +10,31 @@ package com.example.interview.juc.lockSupport;
 public class LockSupportDemo {
     public static void main(String[] args) {
 
+        Object o = new Object();
+
+        new Thread(() -> {
+            synchronized (o) {
+                System.out.println(Thread.currentThread().getName() + "\t ------- come in");
+                try {
+                    o.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName() + "\t ------- 被唤醒");
+            }
+        }, "t1").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            synchronized (o) {
+                o.notify();
+                System.out.println(Thread.currentThread().getName() + "\t ------- 发出通知");
+            }
+        }, "t2").start();
     }
 }
