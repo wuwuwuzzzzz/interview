d99b71fc74b13ba0fcb34f02c1f4b3a563cc3327
wxz <759126349@qq.com>
2023-01-03 14:18:30 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/lockSupport/LockSupportDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.lockSupport;
+
+/**
+ *
+ * @author wxz
+ * @date 14:18 2023/1/3
+ */
+public class LockSupportDemo {
+    public static void main(String[] args) {
+
+    }
+}
