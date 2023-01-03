3b0099cf851cdb15ab344bbae7a750b35a8f6cc8
wxz <759126349@qq.com>
2023-01-03 14:34:36 +0800

    优化代码

--- src/main/java/com/example/interview/juc/lockSupport/LockSupport2Demo.java
+++ src/main/java/com/example/interview/juc/lockSupport/LockSupport2Demo.java
@@ -1,12 +1,50 @@
 package com.example.interview.juc.lockSupport;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
- *
  * @author wxz
  * @date 14:30 2023/1/3
  */
 public class LockSupport2Demo {
     public static void main(String[] args) {
 
+        Lock lock = new ReentrantLock();
+
+        Condition condition = lock.newCondition();
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t -------- come in");
+                condition.await();
+                System.out.println(Thread.currentThread().getName() + "\t -------- 被唤醒");
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+
+        }, "t1").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+              condition.signal();
+                System.out.println(Thread.currentThread().getName() + "\t -------- 发出通知");
+            } finally {
+              lock.unlock();
+            }
+
+        }, "t2").start();
     }
 }
