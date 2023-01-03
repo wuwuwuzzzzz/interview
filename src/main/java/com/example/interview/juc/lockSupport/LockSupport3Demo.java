4dbf69f598bca0c3c29e47593df5f8bbbab9acc0
wxz <759126349@qq.com>
2023-01-03 14:50:10 +0800

    优化代码

--- src/main/java/com/example/interview/juc/lockSupport/LockSupport3Demo.java
+++ src/main/java/com/example/interview/juc/lockSupport/LockSupport3Demo.java
@@ -1,5 +1,7 @@
 package com.example.interview.juc.lockSupport;
 
+import java.util.concurrent.locks.LockSupport;
+
 /**
  *
  * @author wxz
@@ -8,5 +10,16 @@ package com.example.interview.juc.lockSupport;
 public class LockSupport3Demo {
     public static void main(String[] args) {
 
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t -------- come in");
+            LockSupport.park();
+            System.out.println(Thread.currentThread().getName() + "\t -------- 被唤醒");
+        }, "t1");
+        t1.start();
+
+        new Thread(() -> {
+            LockSupport.unpark(t1);
+            System.out.println(Thread.currentThread().getName() + "\t -------- 发出通知");
+        }, "t2").start();
     }
 }
