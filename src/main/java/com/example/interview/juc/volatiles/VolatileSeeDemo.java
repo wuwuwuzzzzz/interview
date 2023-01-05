f996f7ba286f15e1d17106fd23221fdf712edc05
wxz <759126349@qq.com>
2023-01-05 14:28:50 +0800

    优化代码

--- src/main/java/com/example/interview/juc/volatiles/VolatileSeeDemo.java
+++ src/main/java/com/example/interview/juc/volatiles/VolatileSeeDemo.java
@@ -1,5 +1,7 @@
 package com.example.interview.juc.volatiles;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author wxz
@@ -11,5 +13,22 @@ public class VolatileSeeDemo {
 
     public static void main(String[] args) {
 
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t  -------- come in");
+            while (flag) {
+
+            }
+            System.out.println(Thread.currentThread().getName() + "\t  -------- 程序停止 ");
+        }, "t1").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        flag = false;
+
+        System.out.println(Thread.currentThread().getName() + "\t  -------- 修改完成");
     }
 }
