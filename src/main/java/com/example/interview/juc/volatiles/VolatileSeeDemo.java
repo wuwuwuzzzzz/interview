3630ef322844e2aa5c31eb4943fda67f8205caa4
wxz <759126349@qq.com>
2023-01-05 14:24:14 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/volatiles/VolatileSeeDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.volatiles;
+
+/**
+ *
+ * @author wxz
+ * @date 14:24 2023/1/5
+ */
+public class VolatileSeeDemo {
+    public static void main(String[] args) {
+
+    }
+}
