299ffe8da0cb388866d5f0bc2fc0cc38e8e04a41
wxz <759126349@qq.com>
2023-01-09 09:50:56 +0800

    优化代码

--- src/main/java/com/example/interview/juc/volatiles/VolatileNoAtomicDemo.java
+++ src/main/java/com/example/interview/juc/volatiles/VolatileNoAtomicDemo.java
@@ -1,5 +1,16 @@
 package com.example.interview.juc.volatiles;
 
+import java.util.concurrent.TimeUnit;
+
+class MyNumber {
+
+    int number;
+
+    public synchronized void addPlusPlus() {
+        number++;
+    }
+}
+
 /**
  *
  * @author wxz
@@ -8,5 +19,22 @@ package com.example.interview.juc.volatiles;
 public class VolatileNoAtomicDemo {
     public static void main(String[] args) {
 
+        MyNumber myNumber = new MyNumber();
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 1001; j++) {
+                    myNumber.addPlusPlus();
+                }
+            }, String.valueOf(i)).start();
+        }
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(myNumber.number);
     }
 }
