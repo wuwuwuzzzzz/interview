a8b41cd6d8d14563089eb0045a5746ddbbb957a6
wxz <759126349@qq.com>
2023-01-09 09:47:46 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/volatiles/VolatileNoAtomicDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.volatiles;
+
+/**
+ *
+ * @author wxz
+ * @date 09:47 2023/1/9
+ */
+public class VolatileNoAtomicDemo {
+    public static void main(String[] args) {
+
+    }
+}
