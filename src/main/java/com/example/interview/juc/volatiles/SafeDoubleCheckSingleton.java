6921db6dbd77e93885e08c2114b5e5ddb5c41cbe
wxz <759126349@qq.com>
2023-01-09 10:36:41 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/volatiles/SafeDoubleCheckSingleton.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.volatiles;
+
+/**
+ *
+ * @author wxz
+ * @date 10:36 2023/1/9
+ */
+public class SafeDoubleCheckSingleton {
+    public static void main(String[] args) {
+
+    }
+}
