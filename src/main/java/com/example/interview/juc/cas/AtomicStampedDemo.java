a8d7d311f43cdf15964d0d700c96011126654f1e
wxz <759126349@qq.com>
2023-01-30 17:11:19 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/cas/AtomicStampedDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.cas;
+
+/**
+ *
+ * @author wxz
+ * @date 17:11 2023/1/30
+ */
+public class AtomicStampedDemo {
+    public static void main(String[] args) {
+
+    }
+}
