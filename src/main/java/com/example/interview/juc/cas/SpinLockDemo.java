ca2029976bda863f5f0d14e443698ac49d19e445
wxz <759126349@qq.com>
2023-01-30 12:07:33 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/cas/SpinLockDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.cas;
+
+/**
+ *
+ * @author wxz
+ * @date 12:07 2023/1/30
+ */
+public class SpinLockDemo {
+    public static void main(String[] args) {
+
+    }
+}
