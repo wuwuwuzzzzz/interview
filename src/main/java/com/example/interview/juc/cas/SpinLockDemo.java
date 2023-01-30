4595e8f339e430790a4ab65c49cef1e1a2dea151
wxz <759126349@qq.com>
2023-01-30 16:56:53 +0800

    优化代码

--- src/main/java/com/example/interview/juc/cas/SpinLockDemo.java
+++ src/main/java/com/example/interview/juc/cas/SpinLockDemo.java
@@ -1,11 +1,59 @@
 package com.example.interview.juc.cas;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
  *
  * @author wxz
  * @date 12:07 2023/1/30
  */
 public class SpinLockDemo {
+
+    AtomicReference<Thread> atomicReference = new AtomicReference<>();
+
+    public void lock() {
+        Thread thread = Thread.currentThread();
+        System.out.println(Thread.currentThread().getName() + "\t" + "come in");
+        while (!atomicReference.compareAndSet(null, thread)) {
+
+        }
+    }
+
+    public void unlock() {
+        Thread thread = Thread.currentThread();
+        atomicReference.compareAndSet(thread, null);
+        System.out.println(Thread.currentThread().getName() + "\t" + "over");
+    }
+
     public static void main(String[] args) {
+
+        SpinLockDemo spinLockDemo = new SpinLockDemo();
+
+        new Thread(() -> {
+            spinLockDemo.lock();
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            spinLockDemo.unlock();
+        }, "A").start();
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            spinLockDemo.lock();
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            spinLockDemo.unlock();
+        }, "B").start();
     }
 }
