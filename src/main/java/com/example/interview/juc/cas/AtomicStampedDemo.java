ed15ed3a959a4dc2bfd5d8a89a7d97c4a6562ad9
wxz <759126349@qq.com>
2023-01-30 17:19:45 +0800

    优化代码

--- src/main/java/com/example/interview/juc/cas/AtomicStampedDemo.java
+++ src/main/java/com/example/interview/juc/cas/AtomicStampedDemo.java
@@ -22,8 +22,14 @@ public class AtomicStampedDemo {
     public static void main(String[] args) {
 
         Book book = new Book(1, "book");
-
         AtomicStampedReference<Book> stampedReference = new AtomicStampedReference<>(book, 1);
         System.out.println(stampedReference.getReference() + "\t" + stampedReference.getStamp());
+
+        Book book2 = new Book(2, "book2");
+        boolean b;
+        b = stampedReference.compareAndSet(book, book2, stampedReference.getStamp(), stampedReference.getStamp() + 1);
+        System.out.println(b + "\t" + stampedReference.getReference() + "\t" + stampedReference.getStamp());
+        b = stampedReference.compareAndSet(book2, book, stampedReference.getStamp(), stampedReference.getStamp() + 1);
+        System.out.println(b + "\t" + stampedReference.getReference() + "\t" + stampedReference.getStamp());
     }
 }
