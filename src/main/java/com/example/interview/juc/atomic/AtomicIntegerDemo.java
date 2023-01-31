127a423f99de1bb11c8c6acb44b4060b5abe65b5
wxz <759126349@qq.com>
2023-01-31 10:33:21 +0800

    优化代码

--- src/main/java/com/example/interview/juc/atomic/AtomicIntegerDemo.java
+++ src/main/java/com/example/interview/juc/atomic/AtomicIntegerDemo.java
@@ -1,11 +1,20 @@
 package com.example.interview.juc.atomic;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+class MyNumber{
+    AtomicInteger counter = new AtomicInteger();
+}
+
 /**
  *
  * @author wxz
  * @date 10:32 2023/1/31
  */
 public class AtomicIntegerDemo {
+
+    public static final int SIZE = 50;
+
     public static void main(String[] args) {
 
     }
