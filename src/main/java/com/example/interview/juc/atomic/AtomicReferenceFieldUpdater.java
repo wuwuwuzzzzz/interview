60c416911f15ac9d1f017d7ab17ceaa90949da0b
wxz <759126349@qq.com>
2023-01-31 14:47:01 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/atomic/AtomicReferenceFieldUpdater.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.atomic;
+
+/**
+ *
+ * @author wxz
+ * @date 14:46 2023/1/31
+ */
+public class AtomicReferenceFieldUpdater {
+    public static void main(String[] args) {
+
+    }
+}
