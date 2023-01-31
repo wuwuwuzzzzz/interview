7f956c2800dc3f05cb8942e4f503838e9cac45b6
wxz <759126349@qq.com>
2023-01-31 11:30:30 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/atomic/AtomicIntegerArrayDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.atomic;
+
+/**
+ *
+ * @author wxz
+ * @date 11:30 2023/1/31
+ */
+public class AtomicIntegerArrayDemo {
+    public static void main(String[] args) {
+
+    }
+}
