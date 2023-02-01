61dbc6b6f5ce57106eb8cf41c9d28c3acda2b3f9
wxz <759126349@qq.com>
2023-02-01 11:17:11 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/atomic/LongAdderApiDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.atomic;
+
+/**
+ *
+ * @author wxz
+ * @date 11:17 2023/2/1
+ */
+public class LongAdderApiDemo {
+    public static void main(String[] args) {
+
+    }
+}
