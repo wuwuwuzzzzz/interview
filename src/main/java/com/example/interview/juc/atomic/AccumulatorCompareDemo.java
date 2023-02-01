b7244fffa786fb06069c87202f072098fc58c537
wxz <759126349@qq.com>
2023-02-01 11:35:50 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/atomic/AccumulatorCompareDemo.java
@@ -0,0 +1,63 @@
+package com.example.interview.juc.atomic;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAccumulator;
+import java.util.concurrent.atomic.LongAdder;
+
+class ClickNumber {
+
+    int number = 0;
+
+    public synchronized void clickBySynchronized() {
+        number++;
+    }
+
+    AtomicLong atomicLong = new AtomicLong(0);
+
+    public void clickByAtomicLong() {
+        atomicLong.getAndIncrement();
+    }
+
+    LongAdder longAdder = new LongAdder();
+
+    public void clickByLongAdder() {
+        longAdder.increment();
+    }
+
+    LongAccumulator longAccumulator = new LongAccumulator((x, y) -> x + y, 0);
+
+    public void clickByLongAccumulator() {
+        longAccumulator.accumulate(1);
+    }
+}
+
+/**
+ *
+ * @author wxz
+ * @date 11:28 2023/2/1
+ */
+public class AccumulatorCompareDemo {
+
+    public static final int _1W = 10000;
+
+    public static final int threadNumber = 50;
+
+    public static void main(String[] args) {
+
+        ClickNumber clickNumber = new ClickNumber();
+        long startTime;
+        long endTime;
+
+        CountDownLatch countDownLatch1 = new CountDownLatch(threadNumber);
+        CountDownLatch countDownLatch2 = new CountDownLatch(threadNumber);
+        CountDownLatch countDownLatch3 = new CountDownLatch(threadNumber);
+        CountDownLatch countDownLatch4 = new CountDownLatch(threadNumber);
+
+        new Thread(() -> {
+
+        }).start();
+
+        System.out.println(endTime - startTime);
+    }
+}
