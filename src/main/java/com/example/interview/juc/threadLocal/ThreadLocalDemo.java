71e044da7acca79a99b6d079086ef534057e508f
wxz <759126349@qq.com>
2023-02-05 21:18:33 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/threadLocal/ThreadLocalDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.threadLocal;
+
+/**
+ *
+ * @author wxz
+ * @date 21:18 2023/2/5
+ */
+public class ThreadLocalDemo {
+    public static void main(String[] args) {
+
+    }
+}
