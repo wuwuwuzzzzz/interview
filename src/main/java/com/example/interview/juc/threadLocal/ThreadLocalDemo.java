4480555fe009aa4d2b06dd51a14655ff6ada2ff5
wxz <759126349@qq.com>
2023-02-05 21:23:23 +0800

    优化代码

--- src/main/java/com/example/interview/juc/threadLocal/ThreadLocalDemo.java
+++ src/main/java/com/example/interview/juc/threadLocal/ThreadLocalDemo.java
@@ -1,5 +1,16 @@
 package com.example.interview.juc.threadLocal;
 
+import java.util.Random;
+
+class House {
+
+    int saleCount;
+
+    public synchronized void saleHouse() {
+        ++saleCount;
+    }
+}
+
 /**
  *
  * @author wxz
@@ -8,5 +19,13 @@ package com.example.interview.juc.threadLocal;
 public class ThreadLocalDemo {
     public static void main(String[] args) {
 
+        House house = new House();
+
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                int size = new Random().nextInt(5);
+                System.out.println(size);
+            }, String.valueOf(i)).start();
+        }
     }
 }
