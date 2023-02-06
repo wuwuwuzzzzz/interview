cbc23c564f4f61d33e81ef206f8dfeec38565855
wxz <759126349@qq.com>
2023-02-06 11:46:05 +0800

    优化代码

--- src/main/java/com/example/interview/juc/reference/PhantomReferenceDemo.java
+++ src/main/java/com/example/interview/juc/reference/PhantomReferenceDemo.java
@@ -1,7 +1,11 @@
 package com.example.interview.juc.reference;
 
 import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -18,5 +22,29 @@ public class PhantomReferenceDemo {
         PhantomReference<MyObject> phantomReference = new PhantomReference<>(myObject, queue);
 
         System.out.println(phantomReference.get());
+
+        List<byte[]> list = new ArrayList<byte[]>();
+
+        new Thread(() -> {
+            while (true) {
+                list.add(new byte[1 * 1024 * 1024]);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(phantomReference.get() + "\t" + "list add ok");
+            }
+        }, "t1").start();
+
+        new Thread(() -> {
+            while (true) {
+                Reference<? extends MyObject> poll = queue.poll();
+                if (poll != null) {
+                    System.out.println("有虚引用对象被回收加入了队列");
+                    break;
+                }
+            }
+        }, "t1").start();
     }
 }
