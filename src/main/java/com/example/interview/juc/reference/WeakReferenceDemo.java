288f9c89d10dd47b5751c8e1fcd16940bbf1d261
wxz <759126349@qq.com>
2023-02-06 11:12:18 +0800

    优化代码

--- src/main/java/com/example/interview/juc/reference/WeakReferenceDemo.java
+++ src/main/java/com/example/interview/juc/reference/WeakReferenceDemo.java
@@ -1,9 +1,17 @@
 package com.example.interview.juc.reference;
 
+import java.lang.ref.WeakReference;
+
 /**
  *
  * @author wxz
  * @date 11:11 2023/2/6
  */
 public class WeakReferenceDemo {
+    public static void main(String[] args) {
+        WeakReference<MyObject> weakReference = new WeakReference<>(new MyObject());
+        System.out.println(weakReference.get());
+        System.gc();
+        System.out.println(weakReference.get());
+    }
 }
