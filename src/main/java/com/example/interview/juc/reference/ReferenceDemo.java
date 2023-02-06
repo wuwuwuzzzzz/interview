1186a51d08d52d2046232f65df1a9234b9b7a577
wxz <759126349@qq.com>
2023-02-06 10:48:46 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/reference/ReferenceDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.reference;
+
+/**
+ *
+ * @author wxz
+ * @date 10:48 2023/2/6
+ */
+public class ReferenceDemo {
+    public static void main(String[] args) {
+
+    }
+}
