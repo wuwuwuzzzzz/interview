dd7a129468affee25111c7fc42d213f3ce9dff0b
wxz <759126349@qq.com>
2023-02-06 11:00:47 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/reference/SoftReferenceDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.reference;
+
+/**
+ *
+ * @author wxz
+ * @date 11:00 2023/2/6
+ */
+public class SoftReferenceDemo {
+    public static void main(String[] args) {
+
+    }
+}
