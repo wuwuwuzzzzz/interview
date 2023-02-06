824e0893f3a5d609de4066c759f95262d02fbb4d
wxz <759126349@qq.com>
2023-02-06 09:38:01 +0800

    优化代码

--- src/main/java/com/example/interview/juc/threadLocal/ThreadLocalDemo2.java
+++ src/main/java/com/example/interview/juc/threadLocal/ThreadLocalDemo2.java
@@ -1,5 +1,17 @@
 package com.example.interview.juc.threadLocal;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class MyData {
+
+    ThreadLocal<Integer> threadLocal = ThreadLocal.withInitial(() -> 0);
+
+    public  void add() {
+        threadLocal.set(1 + threadLocal.get());
+    }
+}
+
 /**
  *
  * @author wxz
@@ -8,5 +20,23 @@ package com.example.interview.juc.threadLocal;
 public class ThreadLocalDemo2 {
     public static void main(String[] args) {
 
+        MyData myData = new MyData();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        try {
+            for (int i = 0; i < 10; i++) {
+                executorService.submit(() -> {
+                    Integer beforeInt = myData.threadLocal.get();
+                    myData.add();
+                    Integer afterInt = myData.threadLocal.get();
+                    System.out.println(Thread.currentThread().getName() + "\t" + beforeInt + "\t" + afterInt);
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            executorService.shutdownNow();
+        }
     }
 }
