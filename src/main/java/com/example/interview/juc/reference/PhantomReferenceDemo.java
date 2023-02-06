c1098afc6287216970a15745015415fbcd59ceaf
wxz <759126349@qq.com>
2023-02-06 11:33:47 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/reference/PhantomReferenceDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.reference;
+
+/**
+ *
+ * @author wxz
+ * @date 11:33 2023/2/6
+ */
+public class PhantomReferenceDemo {
+    public static void main(String[] args) {
+
+    }
+}
