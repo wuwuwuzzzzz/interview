952e169bd88484a1b0f2821938aa4da15f543a0f
wxz <759126349@qq.com>
2023-02-06 15:48:25 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/objecthead/ObjectHeadDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.objecthead;
+
+/**
+ *
+ * @author wxz
+ * @date 15:48 2023/2/6
+ */
+public class ObjectHeadDemo {
+    public static void main(String[] args) {
+
+    }
+}
