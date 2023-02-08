6e1a369609185d5427856f36e3cd23f1d5e26e8b
wxz <759126349@qq.com>
2023-02-08 14:40:28 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/sync/LockBigDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.sync;
+
+/**
+ *
+ * @author wxz
+ * @date 14:40 2023/2/8
+ */
+public class LockBigDemo {
+    public static void main(String[] args) {
+
+    }
+}
