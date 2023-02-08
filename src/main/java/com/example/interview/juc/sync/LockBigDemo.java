d6cd08aa4d5ed0c735a2d071ad1f620a48008b8e
wxz <759126349@qq.com>
2023-02-08 14:42:16 +0800

    优化代码

--- src/main/java/com/example/interview/juc/sync/LockBigDemo.java
+++ src/main/java/com/example/interview/juc/sync/LockBigDemo.java
@@ -6,7 +6,24 @@ package com.example.interview.juc.sync;
  * @date 14:40 2023/2/8
  */
 public class LockBigDemo {
+
+    static Object objectLock = new Object();
     public static void main(String[] args) {
+
+        new Thread(() -> {
+            synchronized (objectLock) {
+                System.out.println("111");
+            }
+            synchronized (objectLock) {
+                System.out.println("222");
+            }
+            synchronized (objectLock) {
+                System.out.println("333");
+            }
+            synchronized (objectLock) {
+                System.out.println("444");
+            }
+        }, "t1").start();
 
     }
 }
