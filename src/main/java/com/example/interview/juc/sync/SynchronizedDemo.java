cb100ed7b7bf611717031e099b2b8827b595941d
wxz <759126349@qq.com>
2023-02-08 11:51:38 +0800

    优化代码

--- src/main/java/com/example/interview/juc/sync/SynchronizedDemo.java
+++ src/main/java/com/example/interview/juc/sync/SynchronizedDemo.java
@@ -2,6 +2,8 @@ package com.example.interview.juc.sync;
 
 import org.openjdk.jol.info.ClassLayout;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author wxz
@@ -10,10 +12,17 @@ import org.openjdk.jol.info.ClassLayout;
 public class SynchronizedDemo {
     public static void main(String[] args) {
 
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         Object o = new Object();
 
-        System.out.println(o.hashCode());
-
-        System.out.println(ClassLayout.parseInstance(o).toPrintable());
+        synchronized (o) {
+            System.out.println(o.hashCode());
+            System.out.println(ClassLayout.parseInstance(o).toPrintable());
+        }
     }
 }
