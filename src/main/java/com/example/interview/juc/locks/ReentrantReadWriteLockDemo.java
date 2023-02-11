0d2e6c81af8a0f0f38946692cc7cc07c4b9c9a65
wxz <759126349@qq.com>
2023-02-11 16:04:07 +0800

    优化代码

--- src/main/java/com/example/interview/juc/locks/ReentrantReadWriteLockDemo.java
+++ src/main/java/com/example/interview/juc/locks/ReentrantReadWriteLockDemo.java
@@ -1,5 +1,55 @@
 package com.example.interview.juc.locks;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+class MyResource {
+
+    Map<String, String> map = new HashMap<>();
+
+    Lock lock = new ReentrantLock();
+
+    ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    public void write(String key, String value) {
+        lock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t" + "正在写入");
+            map.put(key, value);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + "\t" + "写入完成");
+        } finally {
+          lock.unlock();
+        }
+    }
+
+    public void read(String key) {
+        lock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t" + "正在读取");
+            String result = map.get(key);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + "\t" + "完成读取");
+        } finally {
+          lock.unlock();
+        }
+
+    }
+}
+
 /**
  *
  * @author wxz
@@ -8,5 +58,20 @@ package com.example.interview.juc.locks;
 public class ReentrantReadWriteLockDemo {
     public static void main(String[] args) {
 
+        MyResource myResource = new MyResource();
+
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                myResource.write(finalI + "", finalI + "");
+            }, String.valueOf(i)).start();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                myResource.read(finalI + "");
+            }, String.valueOf(i)).start();
+        }
     }
 }
