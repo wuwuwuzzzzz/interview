60b77ee118de1b28253dddc09d51ce3622a76ebb
wxz <759126349@qq.com>
2023-02-11 15:52:38 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/locks/ReentrantReadWriteLockDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.locks;
+
+/**
+ *
+ * @author wxz
+ * @date 15:52 2023/2/11
+ */
+public class ReentrantReadWriteLockDemo {
+    public static void main(String[] args) {
+
+    }
+}
