bd1ce8a11b3df06f3149153acc5962b7cf1806c5
wxz <759126349@qq.com>
2023-02-11 18:27:34 +0800

    优化代码

--- src/main/java/com/example/interview/juc/locks/StampedLockDemo.java
+++ src/main/java/com/example/interview/juc/locks/StampedLockDemo.java
@@ -1,12 +1,74 @@
 package com.example.interview.juc.locks;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.StampedLock;
+
 /**
  *
  * @author wxz
  * @date 18:18 2023/2/11
  */
 public class StampedLockDemo {
+
+    static int number = 37;
+
+    static StampedLock stampedLock = new StampedLock();
+
+    public void write() {
+
+        long stamp = stampedLock.writeLock();
+
+        System.out.println("写线程准备修改");
+
+        try {
+            number = number + 13;
+        } finally {
+            stampedLock.unlockWrite(stamp);
+        }
+
+        System.out.println("写线程修改结束");
+    }
+
+    public void read() {
+
+        long stamp = stampedLock.readLock();
+
+        System.out.println("come in");
+
+        for (int i = 0; i < 4; i++) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("正在读取中...");
+        }
+
+        try {
+            int result = number;
+            System.out.println(result);
+        } finally {
+            stampedLock.unlockRead(stamp);
+        }
+
+    }
+
     public static void main(String[] args) {
 
+        StampedLockDemo stampedLockDemo = new StampedLockDemo();
+
+        new Thread(() -> {
+            stampedLockDemo.read();
+        }, "readThread").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            stampedLockDemo.write();
+        }, "writeThread").start();
     }
 }
