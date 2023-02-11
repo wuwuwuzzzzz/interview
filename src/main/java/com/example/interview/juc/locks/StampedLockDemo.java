a7481c648cac138f764823b50709df4d66d5eda6
wxz <759126349@qq.com>
2023-02-11 18:19:02 +0800

    优化代码

---
+++ src/main/java/com/example/interview/juc/locks/StampedLockDemo.java
@@ -0,0 +1,12 @@
+package com.example.interview.juc.locks;
+
+/**
+ *
+ * @author wxz
+ * @date 18:18 2023/2/11
+ */
+public class StampedLockDemo {
+    public static void main(String[] args) {
+
+    }
+}
