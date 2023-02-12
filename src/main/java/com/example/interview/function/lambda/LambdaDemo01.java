69397fcc85cc017f84fd12376a7d72078eb17303
wxz <759126349@qq.com>
2023-02-12 12:29:30 +0800

    优化代码

--- src/main/java/com/example/interview/function/lambda/LambdaDemo01.java
+++ src/main/java/com/example/interview/function/lambda/LambdaDemo01.java
@@ -1,22 +1,37 @@
 package com.example.interview.function.lambda;
 
+import java.util.function.IntBinaryOperator;
+
 /**
- *
  * @author wxz
  * @date 12:20 2023/2/12
  */
 public class LambdaDemo01 {
     public static void main(String[] args) {
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("新线程中run方法被执行了");
-            }
-        }).start();
+        //        new Thread(new Runnable() {
+        //            @Override
+        //            public void run() {
+        //                System.out.println("新线程中run方法被执行了");
+        //            }
+        //        }).start();
+        //
+        //        new Thread(() -> {
+        //            System.out.println("新线程中run方法被执行了");
+        //        }).start();
 
-        new Thread(() -> {
-            System.out.println("新线程中run方法被执行了");
-        }).start();
+        int i = calculateNum(new IntBinaryOperator() {
+            @Override
+            public int applyAsInt(int left, int right) {
+                return left + right;
+            }
+        });
+        System.out.println(i);
+    }
+
+    public static int calculateNum(IntBinaryOperator operation) {
+        int a = 10;
+        int b = 20;
+        return operation.applyAsInt(a, b);
     }
 }
