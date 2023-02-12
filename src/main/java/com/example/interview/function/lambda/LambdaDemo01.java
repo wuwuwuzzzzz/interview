9c357a8b1df6f84da99e2cd72269eade2cfb3f34
wxz <759126349@qq.com>
2023-02-12 12:46:42 +0800

    优化代码

--- src/main/java/com/example/interview/function/lambda/LambdaDemo01.java
+++ src/main/java/com/example/interview/function/lambda/LambdaDemo01.java
@@ -1,6 +1,7 @@
 package com.example.interview.function.lambda;
 
 import java.util.function.IntBinaryOperator;
+import java.util.function.IntPredicate;
 
 /**
  * @author wxz
@@ -20,13 +21,24 @@ public class LambdaDemo01 {
         //            System.out.println("新线程中run方法被执行了");
         //        }).start();
 
-        int i = calculateNum((int left, int right) -> left + right);
-        System.out.println(i);
+        //        int i = calculateNum((int left, int right) -> left + right);
+        //        System.out.println(i);
+
+//        printNum(value -> value % 2 == 0);
     }
 
     public static int calculateNum(IntBinaryOperator operation) {
         int a = 10;
         int b = 20;
         return operation.applyAsInt(a, b);
+    }
+
+    public static void printNum(IntPredicate predicate) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i : arr) {
+            if (predicate.test(i)) {
+                System.out.println(i);
+            }
+        }
     }
 }
