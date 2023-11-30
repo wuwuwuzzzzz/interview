345dc7317f8bccab1a805c172fa9032993919d3a
wxz <759126349@qq.com>
2023-11-30 16:38:01 +0800

    优化代码

--- src/main/java/com/example/interview/jdk14/Feature02.java
+++ src/main/java/com/example/interview/jdk14/Feature02.java
@@ -1,21 +1,23 @@
 package com.example.interview.jdk14;
 
-import java.util.PriorityQueue;
-
 /**
  * @author wxz
  * @date 11:46 2023/2/15
  */
-public class Feature02 {
-    public static void main(String[] args) {
+public class Feature02
+{
+    public static void main(String[] args)
+    {
 
         String x = "3";
 
-        int num = switch (x) {
+        int num = switch (x)
+        {
             case "1" -> 1;
             case "2" -> 2;
             case "3" -> 3;
-            default -> {
+            default ->
+            {
                 System.out.println("default");
                 yield 4;
             }
